f24c71885ea10ed2ae8ee4cea719633fa9568935
Ivalin Dimitrov Georgiev <ivalin.9009@gmail.com>
2023-11-10 02:26:45 +0000

    Added all the other lessons.

--- lessons.js
+++ lessons.js
@@ -7,5 +7,93 @@ let Lessons = [
     lessonSpaces: 20,
     image: "images/English-Grammar.jpg"
   },
+  {
+    id: 2,
+    lessonName: "Maths Lesson",
+    lessonLocation: "Harrow",
+    lessonPrice: 30,
+    lessonSpaces: 15,
+    image: "images/Maths.jpg"
+  },
+  {
+    id: 3,
+    lessonName: "Science Lesson",
+    lessonLocation: "Hendon",
+    lessonPrice: 20,
+    lessonSpaces: 14,
+    image: "images/12BEBC8F047B65A5B06D6299725B0DB4.jpg"
+  },
+  {
+    id: 4,
+    lessonName: "Business Lesson",
+    lessonLocation: "Hendon",
+    lessonPrice: 25,
+    lessonSpaces: 12,
+    image: "images/business.jpg"
+  },
+  {
+    id: 5,
+    lessonName: "IT Lesson",
+    lessonLocation: "Ruislip",
+    lessonPrice: 40,
+    lessonSpaces: 10,
+    image: "images/IT.jpg"
+  },
+  {
+    id: 6,
+    lessonName: "Computer Science Lesson",
+    lessonLocation: "Ruislip",
+    lessonPrice: 45,
+    lessonSpaces: 16,
+    image: "images/computerScience.jpg"
+  },
+  {
+    id: 7,
+    lessonName: "French Lesson",
+    lessonLocation: "Watford",
+    lessonPrice: 20,
+    lessonSpaces: 8,
+    image: "images/istockphoto-1054778956-612x612.jpg"
+  },
+  {
+    id: 8,
+    lessonName: "German Lesson",
+    lessonLocation: "Harrow",
+    lessonPrice: 25,
+    lessonSpaces: 8,
+    image: "images/st,small,507x507-pad,600x600,f8f8f8.jpg"
+  },
+  {
+    id: 9,
+    lessonName: "Art Lesson",
+    lessonLocation: "Hendon",
+    lessonPrice: 30,
+    lessonSpaces: 15,
+    image: "images/EF635F82EDB9C323A3E8F8CE67598083.jpg"
+  },
+  {
+    id: 10,
+    lessonName: "Music Lesson",
+    lessonLocation: "Ruislip",
+    lessonPrice: 35,
+    lessonSpaces: 20,
+    image: "images/Music.jpg"
+  },
+  {
+    id: 11,
+    lessonName: "Mechanical Engineering",
+    lessonLocation: "Harrow",
+    lessonPrice: 60,
+    lessonSpaces: 8,
+    image: "images/Music.jpg"
+  },
+  {
+    id: 12,
+    lessonName: "Philosophy",
+    lessonLocation: "Wartford",
+    lessonPrice: 40,
+    lessonSpaces: 12,
+    image: "images/Music.jpg"
+  },
 ];
 
