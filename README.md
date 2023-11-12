5e96b19a0edbd69234b71575dac84886b7d8c1a4
Ivalin Dimitrov Georgiev <ivalin.9009@gmail.com>
2023-11-12 18:18:08 +0000

    added the add to cart button butto disabled after lesson spaces 0 lesson spaces updated after button pushed

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CST3145-Lesson-Booking-System
 https://github.com/ivalin01/CST3145-Lesson-Booking-System/edit/main/README.md
 
+https://www.youtube.com/watch?v=11AoMCAImtE&t=232s
--- app.js
+++ app.js
@@ -1,7 +1,56 @@
 let lesson = new Vue({
     el: "#list-of-lessons",
     data: {
-        Lessons: Lessons,
+        show: true,
+        allLessons: Lessons,
+        cart: [],
+        isCheckoutVisible: false,
+        order: {
+            FullName: "",
+            PhoneNumber: "",
+        }
+    },
+    methods: {
+
+        //function to push lessons in the cart and update the spaces available
+        addItemToCart: function (subject) {
+            if (subject.lessonSpaces > 0) {
+                this.cart.push({
+                    id: subject.id,
+                    name: subject.lessonName,
+                    location: subject.lessonLocation,
+                    price: subject.lessonPrice
+                });
+                subject.lessonSpaces--;
+            }
+
+        },
+
+        /*function to count number of items per lesson id so that each lesson can be added to
+        the cart the specified amout of times in the lessons.js 
+        */
+        cartCount(id) {
+            let count = 0;
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    count++;
+                }
+
+            }
+            return count;
+        },
+        //function to check if item can be added to the cart to based on the lesson spaces
+        canAddInCart(subject) {
+            return subject.lessonSpaces > this.cartCount(subject.id);
+        },
+    },
+
+    computed: {
+        //function to return the number of items in the cart
+        itemsInTheCart: function () {
+            return this.cart.length || "";
+        },
+
     },
 
 });
